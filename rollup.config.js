903faae8243e83e157050734761a7da93d082295
Josselin BUILS <josselin.buils@gmail.com>
2020-04-04 20:59:27 +0200

    Cleans stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import autoprefixer from 'autoprefixer';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import analyze from 'rollup-plugin-analyzer';
 import multiInput from 'rollup-plugin-multi-input';
 import postcss from 'rollup-plugin-postcss';
 import typescript from 'rollup-plugin-typescript2';
@@ -10,7 +9,7 @@ export default {
   input: ['src/**/index.ts', '!src/doc'],
   output: {
     dir: 'dist',
-    format: 'esm'
+    format: 'esm',
   },
   external: ['react', 'react-dom'],
   plugins: [
@@ -22,8 +21,7 @@ export default {
       autoModules: true,
       plugins: [autoprefixer()],
       use: ['sass'],
-      extensions: ['.scss', '.css']
+      extensions: ['.scss', '.css'],
     }),
-    process.argv.includes('--analyze') && analyze()
-  ].filter(Boolean)
+  ],
 };
