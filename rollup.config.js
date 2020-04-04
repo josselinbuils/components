2d35cb74c03f5e30bb1c82ebe3e976451168d473
Josselin BUILS <josselin.buils@gmail.com>
2020-04-04 20:36:48 +0200

    Adds husky and dev tools

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import autoprefixer from 'autoprefixer';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
+import analyze from 'rollup-plugin-analyzer';
 import multiInput from 'rollup-plugin-multi-input';
 import postcss from 'rollup-plugin-postcss';
 import typescript from 'rollup-plugin-typescript2';
@@ -22,6 +23,7 @@ export default {
       plugins: [autoprefixer()],
       use: ['sass'],
       extensions: ['.scss', '.css']
-    })
-  ]
+    }),
+    process.argv.includes('--analyze') && analyze()
+  ].filter(Boolean)
 };
